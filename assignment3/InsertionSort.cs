62673d783bcfbe3eae479f10ebb3fc1826a894ed
babujyan <31545427+babujyan@users.noreply.github.com>
2018-02-11 01:40:01 +0400

    InsertionSort

--- assignment3/InsertionSort.cs
+++ assignment3/InsertionSort.cs
@@ -10,11 +10,21 @@ namespace assignment3
     {
         public static int[] Sort(InitArr arr)
         {
-            int[] bubbleArr = arr.Arr();
-            int size = bubbleArr.Length;
+            int[] array = arr.Arr();
+            int size = array.Length;
+            for(int i = 1; i < array.Length; ++i)
+            {
+                int j = i;
+                while(j > 0 && array[j-1] > array[j])
+                {
+                    array[j] = array[j] + array[j-1];
+                    array[j-1] = array[j] - array[j-1];
+                    array[j] = array[j] - array[j-1];
+                    j--;
+                }
+            }
 
-
-            return bubbleArr;
+            return array;
         }
     }
 }
