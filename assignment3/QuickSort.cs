d02508f17f5610e32a391e943693e73ab2df6674
babujyan <31545427+babujyan@users.noreply.github.com>
2018-02-11 02:46:48 +0400

    QuickSort with some errors

--- assignment3/QuickSort.cs
+++ assignment3/QuickSort.cs
@@ -10,11 +10,50 @@ namespace assignment3
     {
         public static int[] Sort(InitArr arr)
         {
-            int[] bubbleArr = arr.Arr();
-            int size = bubbleArr.Length;
-
-
-            return bubbleArr;
+            int[] array = arr.Arr();
+            array = Partition(array, 0, array.Length - 1);
+            int k = 0;
+            while (k < array.Length)
+            {
+                Console.WriteLine(array[k]);
+                k++;
+            }
+            return array;
         }
+        static int[] Partition(int[] array, int low, int high)
+        {
+            int pivot = high;
+            int j = high - 1;
+            int i = low;
+            while (j>=i)
+            {
+
+                while (array[pivot] <= array[j] && j >= i)
+                {
+                    array[pivot] = array[pivot] + array[j];
+                    array[j] = array[pivot] - array[j];
+                    array[pivot] = array[pivot] - array[j];
+                    pivot = j;
+                    j--;
+                }
+                if (array[pivot] <= array[i] && i < j)
+                {
+                    int tamp = array[pivot];
+                    array[pivot] = array[i];
+                    array[i] = array[j];
+                    array[j] = tamp;
+                    pivot = j;
+                    i++;
+                    j--;
+                }
+            }
+            if(low < pivot)
+                Partition(array, low, pivot - 1);
+            if(high > pivot)
+                Partition(array, pivot + 1, high);
+     
+            return array;
+        }
+
     }
 }
